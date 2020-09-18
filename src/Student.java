a09d89d1399d3a84cd21780dc0a57058c679070c
FlowerPowerShower <56584705+FlowerPowerShower@users.noreply.github.com>
2020-09-18 13:01:49 +0200

    HEELLLLO

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,12 @@
 
 public class Student {
+	private String studentID;
 
+	public String getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
 }
