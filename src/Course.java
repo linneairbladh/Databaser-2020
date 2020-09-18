3fca49f906b27d632caaa03266e70d991bd738d4
FlowerPowerShower <56584705+FlowerPowerShower@users.noreply.github.com>
2020-09-18 14:31:16 +0200

    Update Course.java
    SQL connection

--- src/Course.java
+++ src/Course.java
@@ -1,9 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Course {
 
 	private String katter; 
 
 	private String itsFridayThen;
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-}
+	try {
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		String url = "jdbc:sqlserver://LAPTOP-K0LPK14A:1433; database=sqldepartment1";
+		String user = "user3";
+		String password = "sql";
+		Connection con = DriverManager.getConnection(url,user,password);
+		
+		String query = "SELECT * FROM Employee";
+		
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()){
+			System.out.println(rs.getString(2));
+		}
+		
+		
+		
+	} catch (ClassNotFoundException | SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+		
+	}
+
+}	
+
+
