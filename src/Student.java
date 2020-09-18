bed8b7df5ab739023531f2c204c8f9eee1523e75
emmaivarsson <emmaivarsson@users.noreply.github.com>
2020-09-18 13:35:36 +0200

    Added attribute to Student
    Attribute + setters and getters

--- src/Student.java
+++ src/Student.java
@@ -2,11 +2,21 @@
 public class Student {
 
 	private String studentID;
+	private String studentName;
+	
 	public String getStudentID() {
 		return studentID;
 	}
 
 	public void setStudentID(String studentID) {
 		this.studentID = studentID;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
 	}
 }
