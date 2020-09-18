2e8487bab1b9aff3c4645604f661d7530eab3da7
FlowerPowerShower <56584705+FlowerPowerShower@users.noreply.github.com>
2020-09-18 14:49:01 +0200

    Created databas shit

--- src/Course.java
+++ src/Course.java
@@ -42,12 +42,12 @@ public class Course {
 
 	try {
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		String url = "jdbc:sqlserver://LAPTOP-K0LPK14A:1433; database=sqldepartment1";
-		String user = "user3";
-		String password = "sql";
+		String url = "jdbc:sqlserver://localhost:1433;database=University";
+		String user = "useradmin";
+		String password = "password";
 		Connection con = DriverManager.getConnection(url,user,password);
 		
-		String query = "SELECT * FROM Employee";
+		String query = "SELECT * FROM Student";
 		
 		PreparedStatement ps = con.prepareStatement(query);
 		
