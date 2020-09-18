93efdc7dbd8b21e9123915fe0de5d010096693e5
MonnyPonny <56629015+MonnyPonny@users.noreply.github.com>
2020-09-18 14:05:23 +0200

    Return of Studentmannen
    Väldigt viktigt

--- src/Student.java
+++ src/Student.java
@@ -3,7 +3,7 @@ public class Student {
 	private String studentID;
 	private String studentName;
 	private String studentMannnen;
-	
+	private String studentMannen2;
 	public String getStudentID() {
 		return studentID;
 	}
