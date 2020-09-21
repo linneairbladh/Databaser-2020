a17709fdd657d1e69bc277b046312331a4238565
linneairbladh <56582158+linneairbladh@users.noreply.github.com>
2020-09-21 12:39:45 +0200

    Database Connection

--- src/CourseData.java
+++ src/CourseData.java
@@ -1,4 +1,14 @@
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 public class CourseData {
 
+
+	public ArrayList<Course> getAllCourses() {
+		
+		String query = "SELECT * FROM Student";
+		
+		PreparedStatement ps = con.prepareStatement(query);
+	}
+	
 }
