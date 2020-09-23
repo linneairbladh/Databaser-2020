0df640f3f6a3f9b7f37475958e384c91fb59b21a
MonnyPonny <56629015+MonnyPonny@users.noreply.github.com>
2020-09-23 12:12:49 +0200

    AllStudents arraylist

--- src/DAL.java
+++ src/DAL.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
@@ -14,7 +15,7 @@ public class DAL {
 	private static String password = "password";
 	
 	private Connection conn = null;
-	//hej
+	
 	private Connection sqlConn() throws SQLException {
 			try { 
 				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -25,114 +26,15 @@ public class DAL {
 			return conn;
 	}
 
-	//Hitta kurs
-	public ResultSet getCourse(String courseCode) throws SQLException {
-	
-		conn = sqlConn();
-		String query = "SELECT * FROM COURSE";	
-		PreparedStatement ps = conn.prepareStatement(query);
-		
-		ResultSet resultSet = ps.executeQuery();
-		return resultSet;	
-	}
-	
-	//Hitta student
-	public ResultSet getStudent(String studentSSN, String studentName) throws SQLException {
-		
-		conn = sqlConn();
-		String query = "SELECT * FROM Student";	
-		PreparedStatement ps = conn.prepareStatement(query);
-		
-		ResultSet resultSet = ps.executeQuery();
-		return resultSet;	
-	}
-	
-
-	//Lägg till kurs
-	public void InsertCourse(String courseCode, String courseName, double credits) throws SQLException {
-		conn = sqlConn();
-		String query = "INSERT INTO Course VALUES('" +courseCode + "', '" +  courseName + "','" + credits + "')";
-
-	public void InsertCourse(String courseCode, String courseName, String cCredits) throws SQLException {
-		conn = sqlConn();
-		String query = "INSERT INTO Course VALUES(' " +courseCode + "', '" +  courseName + "'," + cCredits + ")";
-
-		System.out.println(query);
-		PreparedStatement ps = conn.prepareStatement(query);
-		ps.executeUpdate();
-	}
-	
-
-	//Lägg till student
-	public void InsertStudent(String studentSSN, String studentName) throws SQLException {
-		conn = sqlConn();
-		String query = "INSERT INTO Student VALUES(' " +studentSSN + "', '" +  studentName +")";
-
-	public void InsertStudent(String ssn, String studentName) throws SQLException {
-		conn = sqlConn();
-		String query = "INSERT INTO Student VALUES(' " +ssn + "', '" +  studentName + "',"  +")";
-
-		System.out.println(query);
-		PreparedStatement ps = conn.prepareStatement(query);
-		ps.executeUpdate();
-	}
-
-
-	
-	//Registrera kurs på student
-	public void InsertCourseStudent(String studentSsn, String courseCode) throws SQLException {
-		conn = sqlConn();
-		String query = "INSERT INTO Studies VALUES('" + studentSsn + "', '" +  courseCode +"')";
-		System.out.println(query);
-		PreparedStatement ps = conn.prepareStatement(query);
-		ps.executeUpdate();
-	}
-	
-	//Registrera avklarad kurs på student
-	public void InsertFinishedCourseStudent(String studentSsn, String courseCode, String grade) throws SQLException {
-	conn = sqlConn();
-	String query = "INSERT INTO HasStudied VALUES('" + studentSsn + "', '" +  courseCode + "','" + grade + "')";
-	System.out.println(query);
-	PreparedStatement ps = conn.prepareStatement(query);
-	ps.executeUpdate();
-	}
-
-	
-	public String addNewStudent(String[] student) {
-		return "INSERT INTO Student VALUES(?, ?, ?)";
-	}
-	
-	
-	
-	//metod för att lägga till kurs, Adam och måns version
-	public boolean addCourse(Course c) throws SQLException{
-		String courseCode = c.getCourseCode();
-		String courseName = c.getCourseName();
-		double credits = c.getCredits();
-		
-		Connection con = null;
-		PreparedStatement state = null;
+	public ArrayList<Student> AllStudents() throws SQLException {
+		PreparedStatement sql = null;
+		Connection conn = null;
 		
 		try {
-			con = SqlCon.getConnection();
-			state = con.prepareStatement("Insert into Course values(?,?,?)");
 			
-			state.setString(1, courseCode);
-			state.setString(2, courseName);
-			state.setDouble(3, credits);
-			
-			 int row = state.executeUpdate();
-	            if (row == 1) {
-	                return true;
-	         
 		}
-		
-        return false;
-    } finally {
-        SqlCon.closeResources(con, state);
-    }
-}
-		
+	}
+	
 		
 
 }
